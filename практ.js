b9fc68398304917dd15d60b488f23bf9434024f3
Denis4740 <den.lobov.07@bk.ru>
2025-05-29 23:53:54 +0300

    Add files via upload

---
+++ практ.js
@@ -0,0 +1,12 @@
+//Вывести в консоль соседний элемент h2
+console.log(p.previousElementSibling); 
+
+//Вывести в консоль родительский элемент content
+console.log(p.parentElement); 
+
+//Вывести в консоль картинку
+
+console.log(document.querySelector('png')); 
+
+//Вывести в консоль родительский элемент elem
+console.log(p.parentElement.parentElement); 
